d84df77ac08088f4645078bc1647a409a5aff893
GregC <gcampecino@gmail.com>
2025-05-16 19:49:26 +0800

    cors

--- ec2_python_djo_deploy/settings.py
+++ ec2_python_djo_deploy/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['172.17.0.2']
+CORS_ALLOWED_ORIGINS = [
+    "http://54.91.171.243:81",
+]
